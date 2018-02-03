1a731758ae1d4831e6d2094037d5430ff2d52861
Tobias Lückel <lueckel@solutiondrive.de>
2018-02-03 17:10:25 +0100

    Added ProductBundle doctrine resource and extend the setup context

--- tests/Behat/Context/ProductBundlesSetupContext.php
+++ tests/Behat/Context/ProductBundlesSetupContext.php
@@ -13,16 +13,39 @@ namespace Tests\SolutionDrive\SyliusProductBundlesPlugin\Behat\Context;
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Tester\Exception\PendingException;
+use Sylius\Behat\Service\SharedStorage;
+use Sylius\Component\Resource\Factory\FactoryInterface;
 
 class ProductBundlesSetupContext implements Context
 {
+    /** @var SharedStorage */
+    private $sharedStorage;
+
+    /** @var FactoryInterface */
+    private $productBundleFactory;
 
     /**
-     * @todo not yet implemented
-     * @Given /^the store has a product bundle "([^"]*)"$/
+     * @param SharedStorage $sharedStorage
      */
-    public function theStoreHasAProductBundle($arg1)
+    public function __construct(
+        SharedStorage $sharedStorage,
+        FactoryInterface $productBundleFactory
+    ) {
+        $this->sharedStorage = $sharedStorage;
+        $this->productBundleFactory = $productBundleFactory;
+    }
+
+    /**
+     * @Given the store has a product bundle :name
+     */
+    public function theStoreHasAProductBundle(string $name): void
     {
+        $productBundle = $this->productBundleFactory->createNew();
+        $productBundle->setName($name);
+
+        //@todo persist and flush
+
+        $this->sharedStorage->set('product_bundle', $productBundle);
     }
 
     /**
