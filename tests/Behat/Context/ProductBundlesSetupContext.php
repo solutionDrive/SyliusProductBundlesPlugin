f773f38f44a83a7cba479b69c976f68e56eaed3f
Matthias Alt <alt@solutiondrive.de>
2018-02-14 22:43:06 +0100

    removed unused references and added blank line at the end of suites.yml

--- tests/Behat/Context/ProductBundlesSetupContext.php
+++ tests/Behat/Context/ProductBundlesSetupContext.php
@@ -17,7 +17,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Sylius\Behat\Service\SharedStorage;
 use Sylius\Component\Core\Model\ProductInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
-use Sylius\Component\Resource\Repository\RepositoryInterface;
 
 class ProductBundlesSetupContext implements Context
 {
