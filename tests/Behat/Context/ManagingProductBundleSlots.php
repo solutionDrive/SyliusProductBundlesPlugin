045d7ea1e5e7ff779153f367615493a30534822d
Matthias Alt <alt@solutiondrive.de>
2018-02-16 10:41:31 +0100

    added context-service for managing slots of product bundles

---
+++ tests/Behat/Context/ManagingProductBundleSlots.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Created by solutionDrive GmbH
+ *
+ * @copyright 2018 solutionDrive GmbH
+ */
+
+namespace Tests\SolutionDrive\SyliusProductBundlesPlugin\Behat\Context;
+
+use Behat\Behat\Context\Context;
+
+class ManagingProductBundleSlots implements Context
+{
+
+}
