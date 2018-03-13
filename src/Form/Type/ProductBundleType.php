bfbc5d594640d9c693699f06fa5a9a5947dc52dd
Tobias Wojtylak <tw@solutiondrive.de>
2018-03-13 15:22:49 +0100

    Fixed ProductBundle form: Removed fields name and code

--- src/Form/Type/ProductBundleType.php
+++ src/Form/Type/ProductBundleType.php
@@ -12,8 +12,6 @@ namespace solutionDrive\SyliusProductBundlesPlugin\Form\Type;
 
 use Sylius\Bundle\ProductBundle\Form\Type\ProductChoiceType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SearchType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ProductBundleType extends AbstractType
@@ -21,8 +19,6 @@ class ProductBundleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', SearchType::class, ['label' => 'sylius.ui.name'])
-            ->add('code', TextType::class, ['label' => 'sylius.ui.code'])
             ->add('product', ProductChoiceType::class, ['label' => 'sylius.ui.product'])
         ;
     }
