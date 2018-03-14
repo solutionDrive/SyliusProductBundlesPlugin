3e407993fe0c6e2b22d6974ad21ea0f797dbd1ef
Tobias Wojtylak <tw@solutiondrive.de>
2018-03-14 17:03:34 +0100

    Fixed namespace for ProductBundlesContext

--- tests/Behat/Context/Transform/ProductBundlesContext.php
+++ tests/Behat/Context/Transform/ProductBundlesContext.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests\solutionDrive\SyliusExamplePlugin\Behat\Context\Transform;
+namespace Tests\solutionDrive\SyliusProductBundlesPlugin\Behat\Context\Transform;
 
 use Behat\Behat\Context\Context;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
