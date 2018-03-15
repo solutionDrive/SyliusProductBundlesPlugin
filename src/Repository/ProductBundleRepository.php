f42489f98d6c8ab13d1f7a97298470f95cc6b1e2
Tobias Wojtylak <tw@solutiondrive.de>
2018-03-15 09:24:18 +0100

    Added ProductBundleRepository

---
+++ src/Repository/ProductBundleRepository.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * Created by solutionDrive GmbH
+ *
+ * @copyright 2018 solutionDrive GmbH
+ */
+
+namespace solutionDrive\SyliusProductBundlesPlugin\Repository;
+
+use solutionDrive\SyliusProductBundlesPlugin\Entity\ProductBundleInterface;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+
+class ProductBundleRepository extends EntityRepository implements ProductBundleRepositoryInterface
+{
+    /**
+     * @param string $name
+     * @param string $locale
+     *
+     * @return array|ProductBundleInterface[]
+     */
+    public function findByName(string $name, string $locale): array
+    {
+        return $this->createQueryBuilder('b')
+            ->innerJoin('b.product', 'o')
+            ->innerJoin('o.translations', 'translation', 'WITH', 'translation.locale = :locale')
+            ->andWhere('translation.name = :name')
+            ->setParameter('name', $name)
+            ->setParameter('locale', $locale)
+            ->getQuery()
+            ->getResult()
+            ;
+    }
+}
