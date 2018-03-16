621df1b7c7d40bdd1ce42631b1f15f68c30934ea
Tobias Wojtylak <tw@solutiondrive.de>
2018-03-16 09:52:20 +0100

    Fixed copyright header

--- tests/Behat/Page/ProductBundles/IndexPage.php
+++ tests/Behat/Page/ProductBundles/IndexPage.php
@@ -1,16 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mattagohni
- * Date: 03.02.18
- * Time: 15:16
- */
 declare(strict_types=1);
 
 namespace Tests\solutionDrive\SyliusProductBundlesPlugin\Behat\Page\ProductBundles;
 
 use Sylius\Behat\Page\Admin\Crud\IndexPage as CrudIndexPage;
 
+/**
+ * Created by solutionDrive GmbH
+ *
+ * @copyright 2018 solutionDrive GmbH
+ */
 class IndexPage extends CrudIndexPage
 {
 }
