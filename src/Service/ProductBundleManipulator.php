60796d565dc98925fb365ee7dc2a27738c481ad4
Matthias Alt <alt@solutiondrive.de>
2018-04-04 12:02:42 +0200

    added Service for manipulating ProductBundles, so the code doesn't have to be duplicated in BundlesCreator and BundlesUpdater

---
+++ src/Service/ProductBundleManipulator.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace solutionDrive\SyliusProductBundlesPlugin\Service;
+
+use solutionDrive\SyliusProductBundlesPlugin\Entity\ProductBundleInterface;
+use solutionDrive\SyliusProductBundlesPlugin\Entity\ProductBundleSlotInterface;
+use solutionDrive\SyliusProductBundlesPlugin\Service\Options\ProductBundleSlotOptionsInterface;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+
+class ProductBundleManipulator implements ProductBundleManipulatorInterface
+{
+    /** @var FactoryInterface */
+    private $productBundleSlotFactory;
+
+    /** @var ProductBundleInterface */
+    private $productBundle;
+
+    public function __construct(FactoryInterface $productBundleSlotFactory)
+    {
+        $this->productBundleSlotFactory = $productBundleSlotFactory;
+    }
+
+    public function setProductBundle(ProductBundleInterface $productBundle): void
+    {
+        $this->productBundle = $productBundle;
+    }
+
+    public function getProductBundle(): ?ProductBundleInterface
+    {
+        return $this->productBundle;
+    }
+
+    /**
+     * @param ProductInterface[] $products
+     */
+    public function addSlot(
+        string $slotName,
+        ProductBundleSlotOptionsInterface $options = null,
+        array $products = []
+    ): void
+    {
+        $slot = $this->createSlot($slotName);
+        $this->applyOptionsToSlot($options, $slot);
+        $this->addProductsToSlot($products, $slot);
+        $this->addSlotToBundle($slot);
+    }
+
+    private function createSlot(string $slotName): ProductBundleSlotInterface
+    {
+        /** @var ProductBundleSlotInterface $slot */
+        $slot = $this->productBundleSlotFactory->createNew();
+        $slot->setName($slotName);
+
+        return $slot;
+    }
+
+    private function applyOptionsToSlot(
+        ?ProductBundleSlotOptionsInterface $options,
+        ProductBundleSlotInterface $slot
+    ): void {
+        if (null !== $options && null !== $options->getPosition()) {
+            $slot->setPosition($options->getPosition());
+        }
+        if (null !== $options && $options->isPresentationSlot()) {
+            $this->setPresentationSlotOnBundle($slot);
+        }
+    }
+
+    private function addSlotToBundle(ProductBundleSlotInterface $slot): void
+    {
+        $this->productBundle->addSlot($slot);
+    }
+
+    /**
+     * @param ProductBundleSlotInterface $slot
+     */
+    private function setPresentationSlotOnBundle(ProductBundleSlotInterface $slot): void
+    {
+        $this->productBundle->setPresentationSlot($slot);
+    }
+
+    /**
+     * @param ProductInterface[] $products
+     */
+    private function addProductsToSlot(array $products, ProductBundleSlotInterface $slot): void
+    {
+        foreach ($products as $product) {
+            $this->addProductToSlot($product, $slot);
+        }
+    }
+
+    private function addProductToSlot(ProductInterface $product, ProductBundleSlotInterface $slot): void
+    {
+        $slot->addProduct($product);
+    }
+}
---
+++ src/Service/ProductBundleManipulatorInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Created by solutionDrive GmbH
+ *
+ * @copyright 2018 solutionDrive GmbH
+ */
+
+namespace solutionDrive\SyliusProductBundlesPlugin\Service;
+
+use solutionDrive\SyliusProductBundlesPlugin\Entity\ProductBundleInterface;
+use solutionDrive\SyliusProductBundlesPlugin\Service\Options\ProductBundleSlotOptionsInterface;
+use Sylius\Component\Core\Model\ProductInterface;
+
+interface ProductBundleManipulatorInterface
+{
+    public function setProductBundle(ProductBundleInterface $productBundle): void;
+
+    public function getProductBundle(): ?ProductBundleInterface;
+
+    /**
+     * @param ProductInterface[] $products
+     */
+    public function addSlot(
+        string $slotName,
+        ProductBundleSlotOptionsInterface $options = null,
+        array $products = []
+    ): void;
+}
